3dfd6328c0b2baa041e22b1cec56b54ba0ac85e5
Dmitry Mikhalkov <terilloff@gmail.com>
2020-09-28 01:45:40 +0300

    Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import './Footer.css';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import { Grid, Slider } from '@material-ui/core';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
 
 function Footer() {
   return (
     <div className='footer'>
-      <h1>Footer</h1>
+      <div className='footer__left'>
+        <img className='footer__albumCover' src='https://pyxis.nymag.com/v1/imgs/5a1/58c/580de90bf142c7660dcbaf8faa789a61b1-20-taylor-swift-1989.2x.w710.png' alt='Album Cover'/>
+        <div className='footer__songInfo'>
+          <h4>Shake It Off</h4>
+          <p>Taylor Swift</p>
+        </div>
+      </div>
+      <div className='footer__center'>
+        <ShuffleIcon className='footer__green' />
+        <SkipPreviousIcon className='footer__icon' />
+        <PlayCircleOutlineIcon fontSize='large' className='footer__icon' />
+        <SkipNextIcon className='footer__icon' />
+        <RepeatIcon className='footer__green' />
+      </div>
+      <div className='footer__right'>
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
